5ef74319992d66ef7238e07d310019171d1bb766
Black-Mark <jeshuabay@gmail.com>
2023-04-19 13:45:53 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Axios from 'axios';
+import {useState} from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+  const fetchData = () => {
+    const APIurl = "https://api.agify.io?name="+name;
+    Axios.get(APIurl).then((res) => {
+      setPredictedAge(res.data);
+    })
+  };
+  return (
+    <div className="App">
+      <input 
+      placeholder='Ex. Jeshua...'
+      onChange={(event) => {
+        setName(event.target.value);
+      }}
+      />
+      <button onClick={fetchData}>Predict Age</button>
+      <p>Name: {predictedAge?.name}</p>
+      <p>Predicted Age: {predictedAge?.age}</p>
+      <p>Count: {predictedAge?.count}</p>
+    </div>
+  );
+}
+
+export default App;
